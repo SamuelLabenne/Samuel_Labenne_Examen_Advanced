c6b036b1cf8dbbd3eac8385a29eee995818d5cd3
Sam <Sam@DESKTOP-493I7VD>
2023-08-12 11:35:19 +0200

    Add project files.

---
+++ Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samuel_Labenne_Examen_Advanced
+{
+    internal class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime date { get; set; }
+        public Person[] invited { get; set; }
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Samuel_Labenne_Examen_Advanced
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+    }
+}
